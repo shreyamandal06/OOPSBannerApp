8fe9cb8b4c7aff26f2b84969697cac5c5a8d3b42
Shreya <mandalshreya06@gmail.com>
2026-02-23 13:07:38 +0530

    Add OOPSBannerAppUC4 Java file

--- OOPSBannerAppUC4.java
+++ OOPSBannerAppUC4.java
@@ -9,18 +9,19 @@
  
  public class OOPSBannerAppUC4{
 	 public static void main(String[] args) {
-		 String[] line = new String[7];
 		 
-		 line[0]=String.join(" ","    ***    ","       ***   ","  *******    ","    ******  ");
-		 line[1]=String.join(" "," **     ** ","   **     ** "," **       ** "," **           ");
-         line[2]=String.join(" ","**       **","  **       **"," **      **  ","**            ");
-         line[3]=String.join(" ","**       **","  **       **"," *******    ","    ******    ");
-         line[4]=String.join(" ","**       **","  **       **"," **            ","         **  ");
-         line[5]=String.join(" "," **     ** ","   **     ** "," **            ","        **   ");
-         line[6]=String.join(" ","    ***     ","     ***     ","**             "," ******     ");
+		 String[] lines = new String[7];
 		 
-		 for (String lines : line) {
-            System.out.println(lines);
+		 lines[0]=String.join(" ","    ***    ","       ***   ","  *******    ","    ****** ");
+		 lines[1]=String.join(" "," **     ** ","   **     ** "," **       ** "," **           ");
+         lines[2]=String.join(" ","**       **","  **       **"," **      **  ","**            ");
+         lines[3]=String.join(" ","**       **","  **       **"," *******    ","    ****** ");
+         lines[4]=String.join(" ","**       **","  **       **"," **            ","         **  ");
+         lines[5]=String.join(" "," **     ** ","   **     ** "," **            ","        **   ");
+         lines[6]=String.join(" ","    ***     ","     ***     ","**             "," ******    ");
+		 
+		 for (String line : lines) {
+            System.out.println(line);
         }
     }
 }
